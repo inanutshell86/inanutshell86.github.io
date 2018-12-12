57b4c688090b3b135f86fa3cf314ba1d8acbee10
Ihor Khrystiuk <i.khrystiuk@soft-industry.com>
2018-12-12 15:26:24 +0200

    fix: change the fetch event handler.

--- sw.js
+++ sw.js
@@ -24,20 +24,34 @@ self.addEventListener("activate", e => {
 });
 
 // Call Fetch Event
-self.addEventListener("fetch", e => {
-  console.log("Service Worker: Fetching");
+// self.addEventListener("fetch", e => {
+//   console.log("Service Worker: Fetching");
+//   e.respondWith(
+//     fetch(e.request)
+//       .then(res => {
+//         // Make copy/clone of response
+//         const resClone = res.clone();
+//         // Open cahce
+//         caches.open(cacheName).then(cache => {
+//           // Add response to cache
+//           cache.put(e.request, resClone);
+//         });
+//         return res;
+//       })
+//       .catch(err => caches.match(e.request).then(res => res))
+//   );
+// });
+
+self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Make copy/clone of response
-        const resClone = res.clone();
-        // Open cahce
-        caches.open(cacheName).then(cache => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
+    caches.open(cacheName).then(cache => {
+     return cache.match(e.request).then(response => {
+      return response || fetch(e.request)
+      .then(response => {
+        const responseClone = response.clone();
+        cache.put(e.request, responseClone);
+        })
       })
-      .catch(err => caches.match(e.request).then(res => res))
-  );
+    }
+ );
 });
