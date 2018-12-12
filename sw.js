d4a666b12974a81ff8bc2934e6cd8586cba4a6e8
Ihor Khrystiuk <i.khrystiuk@soft-industry.com>
2018-12-12 17:16:26 +0200

    fix: clean up code.

--- sw.js
+++ sw.js
@@ -23,25 +23,6 @@ self.addEventListener("activate", e => {
   );
 });
 
-// Call Fetch Event
-// self.addEventListener("fetch", e => {
-//   console.log("Service Worker: Fetching");
-//   e.respondWith(
-//     fetch(e.request)
-//       .then(res => {
-//         // Make copy/clone of response
-//         const resClone = res.clone();
-//         // Open cahce
-//         caches.open(cacheName).then(cache => {
-//           // Add response to cache
-//           cache.put(e.request, resClone);
-//         });
-//         return res;
-//       })
-//       .catch(err => caches.match(e.request).then(res => res))
-//   );
-// });
-
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.open(cacheName).then(cache => {
